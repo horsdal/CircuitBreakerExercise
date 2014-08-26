e3bf77d5c0b9e1defb4bd10244a70925b1e4da81
Christian Horsdal <c.horsdal@gmail.com>
2014-08-26 10:55:09 +0200

    stupid ui for calling service

---
+++ CircuitBreakerExercise/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace CircuitBreakerExercise
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private BreakingkService service;
+
+    public MainWindow()
+    {
+      service = new BreakingkService();
+      InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+      this.result.Text = this.service.GetMagicValue();
+    }
+  }
+}
